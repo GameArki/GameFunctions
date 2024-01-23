d3d83502b0dc4ee1edd2577b48bc5450ba7f3bba
Jackutea <chenwansal1@163.com>
2024-01-23 13:32:51 +0800

    <feature> add: MenuTool / Minkowski

---
+++ Assets/Runtime/LinearAlgebra/GFMinkowski2D.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameFunctions {
+
+    public static class GFMinkowski2D {
+
+        /// <summary> return the count of the result polygon </summary>
+        public static int Sum_CulledPolygon(Span<Vector2> aVectors, Span<Vector2> bVectors, Vector2[] culledPolygon) {
+
+            int resCount = 0;
+
+            int aLen = aVectors.Length;
+            int bLen = bVectors.Length;
+
+            int aBottomIndex = BottomIndex(aVectors);
+            int bBottomIndex = BottomIndex(bVectors);
+            int aCurIndex = aBottomIndex;
+            int bCurIndex = bBottomIndex;
+
+            while (resCount < aLen + bLen) {
+
+                culledPolygon[resCount] = aVectors[aCurIndex] + bVectors[bCurIndex];
+                resCount += 1;
+
+                int aNextIndex = (aCurIndex + 1) % aLen;
+                int bNextIndex = (bCurIndex + 1) % bLen;
+
+                float cross = Cross(aVectors[aNextIndex] - aVectors[aCurIndex], bVectors[bNextIndex] - bVectors[bCurIndex]);
+
+                if (cross >= 0 && aCurIndex != aBottomIndex - 1) {
+                    aCurIndex = aNextIndex;
+                } else if (cross <= 0 && bCurIndex != bBottomIndex - 1) {
+                    bCurIndex = bNextIndex;
+                }
+
+            }
+
+            return resCount;
+        }
+
+        /// <summary> return the count of the result polygon </summary>
+        public static int Diff_CulledPolygon(Span<Vector2> aVectors, Span<Vector2> bVectors, Vector2[] culledPolygon) {
+            Span<Vector2> bReversed = stackalloc Vector2[bVectors.Length];
+            for (int i = 0; i < bVectors.Length; i += 1) {
+                bReversed[i] = -bVectors[i];
+            }
+            return Sum_CulledPolygon(aVectors, bReversed, culledPolygon);
+        }
+
+        static float Cross(Vector2 a, Vector2 b) {
+            return a.x * b.y - a.y * b.x;
+        }
+
+        static int BottomIndex(Span<Vector2> vectors) {
+            int res = 0;
+            for (int i = 1; i < vectors.Length; i += 1) {
+                if (vectors[i].y < vectors[res].y || (vectors[i].y == vectors[res].y && vectors[i].x < vectors[res].x)) {
+                    res = i;
+                }
+            }
+            return res;
+        }
+
+        // public static int Sum_FullPolygon(Vector2[] aVectors, Vector2[] bVectors, Vector2[] fullPolygon) {
+        //     int resIndex = 0;
+        //     for (int i = 0; i < aVectors.Length; i += 1) {
+        //         for (int j = 0; j < bVectors.Length; j += 1) {
+        //             fullPolygon[resIndex] = aVectors[i] + bVectors[j];
+        //             resIndex += 1;
+        //         }
+        //     }
+        //     return resIndex;
+        // }
+
+    }
+
+}
---
+++ Assets/Sample/LinearAlgebra/Sample_FPMinkowski.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameFunctions.Sample {
+
+    [ExecuteInEditMode]
+    public class Sample_FPMinkowski : MonoBehaviour {
+
+        [SerializeField] Vector2[] aVectors;
+        [SerializeField] Vector2[] bVectors;
+        Vector2[] results;
+
+        void Awake() {
+            results = new Vector2[10000];
+        }
+
+        // Update is called once per frame
+        void Update() {
+        }
+
+        void OnDrawGizmos() {
+            if (aVectors == null || bVectors == null || aVectors.Length == 0 || bVectors.Length == 0) return;
+            if (results == null || results.Length == 0) {
+                results = new Vector2[1000];
+            }
+            // Draw A
+            Gizmos.color = Color.green;
+            for (int i = 0; i < aVectors.Length; i++) {
+                Vector2 curP = aVectors[i];
+                Vector2 nextP = aVectors[(i + 1) % aVectors.Length];
+                Gizmos.DrawLine(curP, nextP);
+            }
+            // Draw B
+            Gizmos.color = Color.green;
+            for (int i = 0; i < bVectors.Length; i++) {
+                Vector2 curP = bVectors[i];
+                Vector2 nextP = bVectors[(i + 1) % bVectors.Length];
+                Gizmos.DrawLine(curP, nextP);
+            }
+
+            // Draw Sum
+            int sumLength = GFMinkowski2D.Sum_CulledPolygon(aVectors, bVectors, results);
+            Gizmos.color = Color.yellow;
+            for (int i = 0; i < sumLength; i++) {
+                Vector2 point = results[i];
+                Vector2 next = results[(i + 1) % sumLength];
+                Gizmos.DrawLine(point, next);
+            }
+
+            // Draw Diff
+            int resultLength = GFMinkowski2D.Diff_CulledPolygon(aVectors, bVectors, results);
+            Gizmos.color = Color.red;
+            for (int i = 0; i < resultLength; i++) {
+                Vector2 point = results[i];
+                Vector2 next = results[(i + 1) % resultLength];
+                Gizmos.DrawLine(point, next);
+            }
+
+            Gizmos.DrawSphere(Vector2.zero, 1f);
+        }
+    }
+}
