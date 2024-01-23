8c90b1a8a3409de1cf2a6c4b09b3565d4b589101
Jackutea <chenwansal1@163.com>
2024-01-24 00:44:38 +0800

    <feature> 0.2.0 add: GFEasing

--- Assets/Runtime/Easing/GFEasing.cs
+++ Assets/Runtime/Easing/GFEasing.cs
@@ -1,10 +1,142 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace GameFunctions {
 
+    // Attentions:
+    // 1. duration must be greater than 0
+    // 2. duration must greater than passed time
     public static class GFEasing {
+
+        [ThreadStatic] static Dictionary<GFEasingEnum, Func<float, float>> easingFuncs;
+
+        static GFEasing() {
+            easingFuncs = new Dictionary<GFEasingEnum, Func<float, float>>();
+            easingFuncs.Add(GFEasingEnum.Immediate, (float timePercent) => EasingFunctionHelper.EaseImmediate(timePercent));
+            easingFuncs.Add(GFEasingEnum.Linear, (float timePercent) => EasingFunctionHelper.EaseLinear(timePercent));
+            easingFuncs.Add(GFEasingEnum.MountainLinear, (float timePercent) => EasingFunctionHelper.EaseMountainLinear(timePercent));
+            easingFuncs.Add(GFEasingEnum.InQuad, (float timePercent) => EasingFunctionHelper.EaseInQuad(timePercent));
+            easingFuncs.Add(GFEasingEnum.MountainInQuad, (float timePercent) => EasingFunctionHelper.EaseMountainInQuad(timePercent));
+            easingFuncs.Add(GFEasingEnum.OutQuad, (float timePercent) => EasingFunctionHelper.EaseOutQuad(timePercent));
+            easingFuncs.Add(GFEasingEnum.MountainOutQuad, (float timePercent) => EasingFunctionHelper.EaseMountainOutQuad(timePercent));
+            easingFuncs.Add(GFEasingEnum.InOutQuad, (float timePercent) => EasingFunctionHelper.EaseInOutQuad(timePercent));
+            easingFuncs.Add(GFEasingEnum.InCubic, (float timePercent) => EasingFunctionHelper.EaseInCubic(timePercent));
+            easingFuncs.Add(GFEasingEnum.MountainInCubic, (float timePercent) => EasingFunctionHelper.EaseMountainInCubic(timePercent));
+            easingFuncs.Add(GFEasingEnum.OutCubic, (float timePercent) => EasingFunctionHelper.EaseOutCubic(timePercent));
+            easingFuncs.Add(GFEasingEnum.MountainOutCubic, (float timePercent) => EasingFunctionHelper.EaseMountainOutCubic(timePercent));
+            easingFuncs.Add(GFEasingEnum.InOutCubic, (float timePercent) => EasingFunctionHelper.EaseInOutCubic(timePercent));
+            easingFuncs.Add(GFEasingEnum.InQuart, (float timePercent) => EasingFunctionHelper.EaseInQuart(timePercent));
+            easingFuncs.Add(GFEasingEnum.MountainInQuart, (float timePercent) => EasingFunctionHelper.EaseMountainInQuart(timePercent));
+            easingFuncs.Add(GFEasingEnum.OutQuart, (float timePercent) => EasingFunctionHelper.EaseOutQuart(timePercent));
+            easingFuncs.Add(GFEasingEnum.MountainOutQuart, (float timePercent) => EasingFunctionHelper.EaseMountainOutQuart(timePercent));
+            easingFuncs.Add(GFEasingEnum.InOutQuart, (float timePercent) => EasingFunctionHelper.EaseInOutQuart(timePercent));
+            easingFuncs.Add(GFEasingEnum.InQuint, (float timePercent) => EasingFunctionHelper.EaseInQuint(timePercent));
+            easingFuncs.Add(GFEasingEnum.MountainInQuint, (float timePercent) => EasingFunctionHelper.EaseMountainInQuint(timePercent));
+            easingFuncs.Add(GFEasingEnum.OutQuint, (float timePercent) => EasingFunctionHelper.EaseOutQuint(timePercent));
+            easingFuncs.Add(GFEasingEnum.MountainOutQuint, (float timePercent) => EasingFunctionHelper.EaseMountainOutQuint(timePercent));
+            easingFuncs.Add(GFEasingEnum.InOutQuint, (float timePercent) => EasingFunctionHelper.EaseInOutQuint(timePercent));
+            easingFuncs.Add(GFEasingEnum.InSine, (float timePercent) => EasingFunctionHelper.EaseInSine(timePercent));
+            easingFuncs.Add(GFEasingEnum.MountainInSine, (float timePercent) => EasingFunctionHelper.EaseMountainInSine(timePercent));
+            easingFuncs.Add(GFEasingEnum.OutSine, (float timePercent) => EasingFunctionHelper.EaseOutSine(timePercent));
+            easingFuncs.Add(GFEasingEnum.MountainOutSine, (float timePercent) => EasingFunctionHelper.EaseMountainOutSine(timePercent));
+            easingFuncs.Add(GFEasingEnum.InOutSine, (float timePercent) => EasingFunctionHelper.EaseInOutSine(timePercent));
+            easingFuncs.Add(GFEasingEnum.InExpo, (float timePercent) => EasingFunctionHelper.EaseInExpo(timePercent));
+            easingFuncs.Add(GFEasingEnum.OutExpo, (float timePercent) => EasingFunctionHelper.EaseOutExpo(timePercent));
+            easingFuncs.Add(GFEasingEnum.InOutExpo, (float timePercent) => EasingFunctionHelper.EaseInOutExpo(timePercent));
+            easingFuncs.Add(GFEasingEnum.InCirc, (float timePercent) => EasingFunctionHelper.EaseInCirc(timePercent));
+            easingFuncs.Add(GFEasingEnum.MountainInCirc, (float timePercent) => EasingFunctionHelper.EaseMountainInCirc(timePercent));
+            easingFuncs.Add(GFEasingEnum.OutCirc, (float timePercent) => EasingFunctionHelper.EaseOutCirc(timePercent));
+            easingFuncs.Add(GFEasingEnum.MountainOutCirc, (float timePercent) => EasingFunctionHelper.EaseMountainOutCirc(timePercent));
+            easingFuncs.Add(GFEasingEnum.InOutCirc, (float timePercent) => EasingFunctionHelper.EaseInOutCirc(timePercent));
+            easingFuncs.Add(GFEasingEnum.InElastic, (float timePercent) => EasingFunctionHelper.EaseInElastic(timePercent));
+            easingFuncs.Add(GFEasingEnum.OutElastic, (float timePercent) => EasingFunctionHelper.EaseOutElastic(timePercent));
+            easingFuncs.Add(GFEasingEnum.InOutElastic, (float timePercent) => EasingFunctionHelper.EaseInOutElastic(timePercent));
+            easingFuncs.Add(GFEasingEnum.InBack, (float timePercent) => EasingFunctionHelper.EaseInBack(timePercent));
+            easingFuncs.Add(GFEasingEnum.OutBack, (float timePercent) => EasingFunctionHelper.EaseOutBack(timePercent));
+            easingFuncs.Add(GFEasingEnum.InOutBack, (float timePercent) => EasingFunctionHelper.EaseInOutBack(timePercent));
+            easingFuncs.Add(GFEasingEnum.InBounce, (float timePercent) => EasingFunctionHelper.EaseInBounce(timePercent));
+            easingFuncs.Add(GFEasingEnum.OutBounce, (float timePercent) => EasingFunctionHelper.EaseOutBounce(timePercent));
+            easingFuncs.Add(GFEasingEnum.InOutBounce, (float timePercent) => EasingFunctionHelper.EaseInOutBounce(timePercent));
+        }
+
+        public static float Ease1D(GFEasingEnum type, float passTime, float duration, float startValue, float endValue) {
+            float timePercent = passTime / duration;
+            if (timePercent > 1) {
+                timePercent = 1;
+            }
+            float valuePercent = GetValuePercent(type, timePercent);
+            return startValue + (endValue - startValue) * valuePercent;
+        }
+
+        public static Vector2 Ease2D(GFEasingEnum type, float passTime, float duration, Vector2 startValue, Vector2 endValue) {
+            float timePercent = passTime / duration;
+            if (timePercent > 1) {
+                timePercent = 1;
+            }
+            float valuePercent = GetValuePercent(type, timePercent);
+            return startValue + (endValue - startValue) * valuePercent;
+        }
+
+        public static Vector3 Ease3D(GFEasingEnum type, float passTime, float duration, Vector3 startValue, Vector3 endValue) {
+            float timePercent = passTime / duration;
+            if (timePercent > 1) {
+                timePercent = 1;
+            }
+            float valuePercent = GetValuePercent(type, timePercent);
+            return startValue + (endValue - startValue) * valuePercent;
+        }
+
+        /// <summary>
+        /// 3D 球型环绕移动函数.
+        /// <para>解释: 以中心点为球心, 球面上的起始点, 球面上的终点, (起始点-中心点).magnitude 为半径, 环绕移动.</para>
+        /// <returns>返回当前时间在球面上的位置</returns>
+        /// <para name="type">type: 缓动类型</para>
+        /// <para>passTime: 经过的时间</para>
+        /// <para>duration: 总时间</para>
+        /// <para>centerValue: 绕柱的中心点</para>
+        /// <para>startValue: 起始点</para>
+        /// <para>endValue: 终点</para>
+        /// </summary>
+        public static Vector3 Ease3DSphereRound(GFEasingEnum type, float passTime, float duration, Vector3 center, Vector3 startValue, Vector3 endValue, bool isFallbackClockwise = false) {
+            float timePercent = passTime / duration;
+            if (timePercent > 1) {
+                timePercent = 1;
+            }
+            float valuePercent = GetValuePercent(type, timePercent);
+            Vector3 endDiff = (endValue - center);
+            Vector3 startDiff = (startValue - center);
+            float radius = (startValue - center).magnitude;
+            // 1. 计算起始点和终点的夹角 angle
+            // 2. angle = valuePercent * angle
+            // 3. 以 center 为中心, radius 为半径, angle 为角度, 计算出 pos
+            float angle = Vector3.Angle(startDiff, endDiff);
+            if (isFallbackClockwise) {
+                angle = -angle;
+            }
+            Vector3 pos = Quaternion.AngleAxis(angle * valuePercent, Vector3.Cross(startDiff, endDiff)) * startDiff;
+            pos = pos.normalized * radius;
+
+            return center + pos;
+        }
+
+        public static Quaternion EaseQuaternion(GFEasingEnum type, float passTime, float duration, Quaternion startValue, Quaternion endValue) {
+            float timePercent = passTime / duration;
+            if (timePercent > 1) {
+                timePercent = 1;
+            }
+            float valuePercent = GetValuePercent(type, timePercent);
+            return Quaternion.Lerp(startValue, endValue, valuePercent);
+        }
+
+        public static float GetValuePercent(GFEasingEnum type, float timePercent) {
+            easingFuncs.TryGetValue(type, out Func<float, float> func);
+            if (func != null) {
+                return func.Invoke(timePercent);
+            } else {
+                throw new System.ArgumentException("Invalid EasingType" + type.ToString());
+            }
+        }
 
     }
 
---
+++ Assets/Runtime/Easing/Internal/EasingFunctionHelper.cs
@@ -0,0 +1,374 @@
+using System;
+
+namespace GameFunctions {
+
+    // Input: Time Percent
+    // Output: Value Percent
+    internal static class EasingFunctionHelper {
+
+        public static float EaseImmediate(float timePercent) {
+            return 1;
+        }
+
+        // - Linear
+        public static float EaseLinear(float t) {
+            return t;
+        }
+
+        public static float EaseMountainLinear(float t) {
+            if (t < 0.5f) {
+                return EaseLinear(t * 2);
+            } else {
+                return EaseLinear((1 - t) * 2);
+            }
+        }
+
+        // - Quad
+        public static float EaseInQuad(float t) {
+            return t * t;
+        }
+
+        public static float EaseMountainInQuad(float t) {
+            if (t < 0.5f) {
+                return EaseInQuad(t * 2);
+            } else {
+                return EaseInQuad((1 - t) * 2);
+            }
+        }
+
+        public static float EaseOutQuad(float t) {
+            return -t * (t - 2);
+        }
+
+        public static float EaseMountainOutQuad(float t) {
+            if (t < 0.5f) {
+                return EaseOutQuad(t * 2);
+            } else {
+                return EaseOutQuad((1 - t) * 2);
+            }
+        }
+
+        public static float EaseInOutQuad(float t) {
+            t *= 2;
+            if (t < 1) {
+                return 0.5f * t * t;
+            }
+            t -= 1;
+            return -0.5f * (t * (t - 2) - 1);
+        }
+
+        // - Cubic
+        public static float EaseInCubic(float t) {
+            return t * t * t;
+        }
+
+        public static float EaseMountainInCubic(float t) {
+            if (t < 0.5f) {
+                return EaseInCubic(t * 2);
+            } else {
+                return EaseInCubic((1 - t) * 2);
+            }
+        }
+
+        public static float EaseOutCubic(float t) {
+            t -= 1;
+            return (t * t * t + 1);
+        }
+
+        public static float EaseMountainOutCubic(float t) {
+            if (t < 0.5f) {
+                return EaseOutCubic(t * 2);
+            } else {
+                return EaseOutCubic((1 - t) * 2);
+            }
+        }
+
+        public static float EaseInOutCubic(float t) {
+            t *= 2;
+            if (t < 1) {
+                return 0.5f * t * t * t;
+            }
+            t -= 2;
+            return 0.5f * (t * t * t + 2);
+        }
+
+        // - Quart
+        public static float EaseInQuart(float t) {
+            return t * t * t * t;
+        }
+
+        public static float EaseMountainInQuart(float t) {
+            if (t < 0.5f) {
+                return EaseInQuart(t * 2);
+            } else {
+                return EaseInQuart((1 - t) * 2);
+            }
+        }
+
+        public static float EaseOutQuart(float t) {
+            t -= 1;
+            return -(t * t * t * t - 1);
+        }
+
+        public static float EaseMountainOutQuart(float t) {
+            if (t < 0.5f) {
+                return EaseOutQuart(t * 2);
+            } else {
+                return EaseOutQuart((1 - t) * 2);
+            }
+        }
+
+        public static float EaseInOutQuart(float t) {
+            t *= 2;
+            if (t < 1) {
+                return 0.5f * t * t * t * t;
+            }
+            t -= 2;
+            return -0.5f * (t * t * t * t - 2);
+        }
+
+        // - Quint
+        public static float EaseInQuint(float t) {
+            return t * t * t * t * t;
+        }
+
+        public static float EaseMountainInQuint(float t) {
+            if (t < 0.5f) {
+                return EaseInQuint(t * 2);
+            } else {
+                return EaseInQuint((1 - t) * 2);
+            }
+        }
+
+        public static float EaseOutQuint(float t) {
+            t -= 1;
+            return (t * t * t * t * t + 1);
+        }
+
+        public static float EaseMountainOutQuint(float t) {
+            if (t < 0.5f) {
+                return EaseOutQuint(t * 2);
+            } else {
+                return EaseOutQuint((1 - t) * 2);
+            }
+        }
+
+        public static float EaseInOutQuint(float t) {
+            t *= 2;
+            if (t < 1) {
+                return 0.5f * t * t * t * t * t;
+            }
+            t -= 2;
+            return 0.5f * (t * t * t * t * t + 2);
+        }
+
+        // - Sine
+        public static float EaseInSine(float t) {
+            return -MathF.Cos(t * (MathF.PI / 2)) + 1;
+        }
+
+        public static float EaseMountainInSine(float t) {
+            if (t < 0.5f) {
+                return EaseInSine(t * 2);
+            } else {
+                return EaseInSine((1 - t) * 2);
+            }
+        }
+
+        public static float EaseOutSine(float t) {
+            return MathF.Sin(t * (MathF.PI / 2));
+        }
+
+        public static float EaseMountainOutSine(float t) {
+            if (t < 0.5f) {
+                return EaseOutSine(t * 2);
+            } else {
+                return EaseOutSine((1 - t) * 2);
+            }
+        }
+
+        public static float EaseInOutSine(float t) {
+            return -0.5f * (MathF.Cos(MathF.PI * t) - 1);
+        }
+
+        // - Expo
+        public static float EaseInExpo(float t) {
+            return MathF.Pow(2, 10 * (t - 1));
+        }
+
+        public static float EaseOutExpo(float t) {
+            return -MathF.Pow(2, -10 * t) + 1;
+        }
+
+        public static float EaseInOutExpo(float t) {
+            t *= 2;
+            if (t < 1) {
+                return 0.5f * MathF.Pow(2, 10 * (t - 1));
+            }
+            t -= 1;
+            return 0.5f * (-MathF.Pow(2, -10 * t) + 2);
+        }
+
+        // - Circ
+        public static float EaseInCirc(float t) {
+            return -(MathF.Sqrt(1 - t * t) - 1);
+        }
+
+        public static float EaseMountainInCirc(float t) {
+            if (t < 0.5f) {
+                return EaseInCirc(t * 2);
+            } else {
+                return EaseInCirc((1 - t) * 2);
+            }
+        }
+
+        public static float EaseOutCirc(float t) {
+            t -= 1;
+            return MathF.Sqrt(1 - t * t);
+        }
+
+        public static float EaseMountainOutCirc(float t) {
+            if (t < 0.5f) {
+                return EaseOutCirc(t * 2);
+            } else {
+                return EaseOutCirc((1 - t) * 2);
+            }
+        }
+
+        public static float EaseInOutCirc(float t) {
+            t *= 2;
+            if (t < 1) {
+                return -0.5f * (MathF.Sqrt(1 - t * t) - 1);
+            }
+            t -= 2;
+            return 0.5f * (MathF.Sqrt(1 - t * t) + 1);
+        }
+
+        // - Elastic
+        public static float EaseInElastic(float t) {
+            float s = 1.70158f;
+            float p = 0;
+            float a = 1;
+            if (t == 0) {
+                return 0;
+            }
+            if (t == 1) {
+                return 1;
+            }
+            if (p == 0) {
+                p = 0.3f;
+            }
+            if (a < 1) {
+                a = 1;
+                s = p / 4;
+            } else {
+                s = p / (2 * MathF.PI) * MathF.Asin(1 / a);
+            }
+            t -= 1;
+            return -(a * MathF.Pow(2, 10 * t) * MathF.Sin((t - s) * (2 * MathF.PI) / p));
+        }
+
+        public static float EaseOutElastic(float t) {
+            float s = 1.70158f;
+            float p = 0;
+            float a = 1;
+            if (t == 0) {
+                return 0;
+            }
+            if (t == 1) {
+                return 1;
+            }
+            if (p == 0) {
+                p = 0.3f;
+            }
+            if (a < 1) {
+                a = 1;
+                s = p / 4;
+            } else {
+                s = p / (2 * MathF.PI) * MathF.Asin(1 / a);
+            }
+            return a * MathF.Pow(2, -10 * t) * MathF.Sin((t - s) * (2 * MathF.PI) / p) + 1;
+        }
+
+        public static float EaseInOutElastic(float t) {
+            float s = 1.70158f;
+            float p = 0;
+            float a = 1;
+            if (t == 0) {
+                return 0;
+            }
+            t *= 2;
+            if (t == 2) {
+                return 1;
+            }
+            if (p == 0) {
+                p = 0.3f * 1.5f;
+            }
+            if (a < 1) {
+                a = 1;
+                s = p / 4;
+            } else {
+                s = p / (2 * MathF.PI) * MathF.Asin(1 / a);
+            }
+            if (t < 1) {
+                t -= 1;
+                return -0.5f * (a * MathF.Pow(2, 10 * t) * MathF.Sin((t - s) * (2 * MathF.PI) / p));
+            }
+            t -= 1;
+            return a * MathF.Pow(2, -10 * t) * MathF.Sin((t - s) * (2 * MathF.PI) / p) * 0.5f + 1;
+        }
+
+        // - Back
+        public static float EaseInBack(float t) {
+            float s = 1.70158f;
+            return t * t * ((s + 1) * t - s);
+        }
+
+        public static float EaseOutBack(float t) {
+            float s = 1.70158f;
+            t -= 1;
+            return (t * t * ((s + 1) * t + s) + 1);
+        }
+
+        public static float EaseInOutBack(float t) {
+            float s = 1.70158f;
+            t *= 2;
+            if (t < 1) {
+                s *= 1.525f;
+                return 0.5f * (t * t * ((s + 1) * t - s));
+            }
+            t -= 2;
+            s *= 1.525f;
+            return 0.5f * (t * t * ((s + 1) * t + s) + 2);
+        }
+
+        // - Bounce
+        public static float EaseInBounce(float t) {
+            return 1 - EaseOutBounce(1 - t);
+        }
+
+        public static float EaseOutBounce(float t) {
+            if (t < (1 / 2.75f)) {
+                return (7.5625f * t * t);
+            } else if (t < (2 / 2.75f)) {
+                t -= (1.5f / 2.75f);
+                return (7.5625f * t * t + 0.75f);
+            } else if (t < (2.5 / 2.75)) {
+                t -= (2.25f / 2.75f);
+                return (7.5625f * t * t + 0.9375f);
+            } else {
+                t -= (2.625f / 2.75f);
+                return (7.5625f * t * t + 0.984375f);
+            }
+        }
+
+        public static float EaseInOutBounce(float t) {
+            if (t < 0.5f) {
+                return EaseInBounce(t * 2) * 0.5f;
+            }
+            return EaseOutBounce(t * 2 - 1) * 0.5f + 0.5f;
+        }
+
+    }
+
+}
