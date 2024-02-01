14131837b231b4074ca43b66326e7dd31603b4d2
Jackutea <chenwansal1@163.com>
2024-02-01 21:31:50 +0800

    <feature> 0.4.1 add: Camera2D.CalcConfine

---
+++ Assets/Runtime/Camera2D/GFCamera2D.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameFunctions {
+
+    public static class GFCamera2D {
+
+        /// <summary>
+        /// Confine the camera in a rectangle area.
+        /// <para>Return the new camera position.</para>
+        /// <para>resolutionRadio: width / height </para>
+        /// </summary>
+        public static Vector2 CalcConfinePos(Vector2 cameraPos, Vector2 confinerMin, Vector2 confinerMax, float cameraOrthoSize, float resolutionRadio) {
+
+            Vector2 confinerHalfSize = (confinerMax - confinerMin) / 2;
+            Vector2 cameraHalfSize = new Vector2(cameraOrthoSize * resolutionRadio, cameraOrthoSize);
+            if (confinerHalfSize.x < cameraHalfSize.x || confinerHalfSize.y < cameraHalfSize.y) {
+                Debug.LogError("Confiner area is smaller than camera area.");
+                return cameraPos;
+            }
+
+            Vector2 cameraMin = cameraPos - cameraHalfSize;
+            Vector2 cameraMax = cameraPos + cameraHalfSize;
+
+            Vector2 diff = Vector2.zero;
+            if (cameraMin.x < confinerMin.x) {
+                diff.x = confinerMin.x - cameraMin.x;
+            } else if (cameraMax.x > confinerMax.x) {
+                diff.x = confinerMax.x - cameraMax.x;
+            }
+
+            if (cameraMin.y < confinerMin.y) {
+                diff.y = confinerMin.y - cameraMin.y;
+            } else if (cameraMax.y > confinerMax.y) {
+                diff.y = confinerMax.y - cameraMax.y;
+            }
+
+            return cameraPos + diff;
+
+        }
+
+    }
+}
