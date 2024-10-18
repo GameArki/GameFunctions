280065bf10032d327774d9a838b903088d859973
Jackutea <chenwansal1@163.com>
2024-10-18 18:35:20 +0800

    <feature> add: GFSetting - 修改相机的正交尺寸

---
+++ Assets/Runtime/GameFunctions/Setting/GFSetting.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameFunctions {
+
+    public static class GFSetting {
+
+        /// <summary>
+        /// Intro: 该函数保证一屏不会裁剪掉任何东西, 但会留下黑边
+        /// Input: 假设一屏最小值是 40 x 22.5 米, 传入 40, 22.5
+        /// return: 正交相机大小 OrthographicSize
+        /// </summary>
+        public static float CalculateCameraOrthographicSize(float minWidth, float minHeight) {
+            float curAspect = (float)Screen.width / Screen.height;
+            float targetAspect = minWidth / minHeight;
+            float targetOrthographicSize = minHeight;
+            if (curAspect < targetAspect) {
+                // 竖屏
+                targetOrthographicSize = minWidth * 0.5f / curAspect;
+            } else if (curAspect > targetAspect) {
+                // 带鱼屏
+            }
+            return targetOrthographicSize;
+        }
+
+    }
+
+}
