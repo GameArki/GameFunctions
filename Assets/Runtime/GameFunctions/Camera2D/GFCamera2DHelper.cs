fdd32073cc628cdb36cd41d6d6dd15b14da0951c
Jackutea <chenwansal1@163.com>
2024-10-18 21:11:34 +0800

    <feature> modify: Camera2D - Confine 解决方案改为使用最小值

--- Assets/Runtime/GameFunctions/Camera2D/GFCamera2DHelper.cs
+++ Assets/Runtime/GameFunctions/Camera2D/GFCamera2DHelper.cs
@@ -12,27 +12,17 @@ namespace GameFunctions {
         /// <para>resolutionRadio: width / height </para>
         /// </summary>
         public static Vector2 CalcConfinePos(Vector2 cameraPos, Vector2 confinerMin, Vector2 confinerMax, float cameraOrthoSize, float resolutionRadio) {
+            
+            Vector2 target = cameraPos;
 
             Vector2 confinerHalfSize = (confinerMax - confinerMin) / 2;
             Vector2 cameraHalfSize = new Vector2(cameraOrthoSize * resolutionRadio, cameraOrthoSize);
+            cameraHalfSize = Vector2.Min(cameraHalfSize, confinerHalfSize);
 
-            Vector2 cameraMin = cameraPos - cameraHalfSize;
-            Vector2 cameraMax = cameraPos + cameraHalfSize;
+            target.x = Mathf.Clamp(target.x, confinerMin.x + cameraHalfSize.x, confinerMax.x - cameraHalfSize.x);
+            target.y = Mathf.Clamp(target.y, confinerMin.y + cameraHalfSize.y, confinerMax.y - cameraHalfSize.y);
 
-            Vector2 diff = Vector2.zero;
-            if (cameraMin.x < confinerMin.x) {
-                diff.x = confinerMin.x - cameraMin.x;
-            } else if (cameraMax.x > confinerMax.x) {
-                diff.x = confinerMax.x - cameraMax.x;
-            }
-
-            if (cameraMin.y < confinerMin.y) {
-                diff.y = confinerMin.y - cameraMin.y;
-            } else if (cameraMax.y > confinerMax.y) {
-                diff.y = confinerMax.y - cameraMax.y;
-            }
-
-            return cameraPos + diff;
+            return target;
 
         }
 
