700443aa0db6fbec814574759fbc51284b1d874d
Jackutea <chenwansal1@163.com>
2024-11-18 23:36:27 +0800

    <feature> GameRenderer fix: Shader.Find 无法在编译后生效

--- Assets/Runtime/GameRenderer/Pass/NJMScanLinePass.cs
+++ Assets/Runtime/GameRenderer/Pass/NJMScanLinePass.cs
@@ -8,16 +8,16 @@ namespace GameRenderer {
     [Serializable]
     public class NJMScanLinePass : ScriptableRenderPass {
 
-        Shader shader;
+        [SerializeField] Shader shader;
         Material material;
         string renderTag;
         int mainTexID;
         int tempTexID;
         RenderTargetIdentifier currentTarget;
+        RenderTextureDescriptor cameraTextureDescriptor;
 
         public void Setup(RenderTargetIdentifier rt) {
             renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
-            shader = Shader.Find("NJM/Shader_PP_ScanLine");
             material = CoreUtils.CreateEngineMaterial(shader);
             renderTag = "NJM ScanLine";
             mainTexID = Shader.PropertyToID("_MainTex");
@@ -26,7 +26,7 @@ namespace GameRenderer {
         }
 
         public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor) {
-
+            this.cameraTextureDescriptor = cameraTextureDescriptor;
         }
 
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData) {
@@ -53,8 +53,8 @@ namespace GameRenderer {
             var dst = tempTexID;
 
             cmd.SetGlobalTexture(mainTexID, src);
-            cmd.GetTemporaryRT(dst, camera.scaledPixelWidth, camera.scaledPixelHeight,0,FilterMode.Point, RenderTextureFormat.Default);
-            // cmd.GetTemporaryRT(dst, cameraData.cameraTargetDescriptor);
+            // cmd.GetTemporaryRT(dst, camera.scaledPixelWidth, camera.scaledPixelHeight, 0, FilterMode.Point, RenderTextureFormat.Default);
+            cmd.GetTemporaryRT(dst, cameraTextureDescriptor);
             cmd.Blit(src, dst);
             cmd.Blit(dst, src, material, 0);
             context.ExecuteCommandBuffer(cmd);
