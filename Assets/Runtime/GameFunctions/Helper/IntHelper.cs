bbd69caba74e04a868f96d38230f6659c068c041
Jackutea <chenwansal1@163.com>
2024-12-30 01:06:27 +0800

    <version> 0.21.9 <feature> modify: 无gc 数字字符串

--- Assets/Runtime/GameFunctions/Helper/IntHelper.cs
+++ Assets/Runtime/GameFunctions/Helper/IntHelper.cs
@@ -6,17 +6,15 @@ namespace GameFunctions {
     public static class IntHelper {
 
         static Dictionary<int, string> numberToStringNoGC = new Dictionary<int, string>();
-        static IntHelper() {
-            for (int i = 0; i < 10000; i++) {
-                numberToStringNoGC.Add(i, i.ToString());
-            }
-        }
 
-        public static string ToStringNoGC_in10000(int number) {
+        public static string ToStringNoGC(int number) {
             if (numberToStringNoGC.TryGetValue(number, out string result)) {
                 return result;
+            } else {
+                string str = number.ToString();
+                numberToStringNoGC.Add(number, str);
+                return str;
             }
-            return number.ToString();
         }
     }
 }
