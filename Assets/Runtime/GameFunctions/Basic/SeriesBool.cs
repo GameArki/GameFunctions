5f4d1fd8921adb6a1fc941183496db6bd0a5f232
Jackutea <chenwansal1@163.com>
2025-02-09 13:35:20 +0800

    <version> 0.24.9 <feature> add: SeriesBool

---
+++ Assets/Runtime/GameFunctions/Basic/SeriesBool.cs
@@ -0,0 +1,51 @@
+using System;
+
+namespace GameFunctions {
+
+    public struct SeriesBool {
+
+        byte value_current;
+        byte value_on;
+
+        public SeriesBool(params byte[] bits) {
+            value_current = 0;
+            value_on = 0;
+            foreach (var bit in bits) {
+                Register(bit);
+            }
+        }
+
+        // 哪些开关打开后，才算是开启
+        public void Register(byte bitID) {
+            value_on |= (byte)(1 << bitID);
+        }
+
+        // 打开某开关
+        public void On(byte bitID) {
+            value_current |= (byte)(1 << bitID);
+        }
+
+        public void OnAll() {
+            value_current = value_on;
+        }
+
+        // 关闭某开关
+        public void Off(byte bitID) {
+            value_current &= (byte)~(1 << bitID);
+        }
+
+        public void OffAll() {
+            value_current = 0;
+        }
+
+        public bool IsOn() {
+            return value_current == value_on;
+        }
+
+        public static explicit operator bool(SeriesBool me) {
+            return me.IsOn();
+        }
+
+    }
+
+}
