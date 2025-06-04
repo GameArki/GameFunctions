b0d53ac797bc3af5a3a0022c35ae7ae58d8e2bda
Jackutea <chenwansal1@163.com>
2025-06-04 20:28:30 +0800

    <version> 0.31.0 <feature> add: GameObjectHelper.CreateFile_ScriptableObject<T>

--- Assets/Editor/StaticFunctions/GameObjectHelper/GameObjectHelper.cs
+++ Assets/Editor/StaticFunctions/GameObjectHelper/GameObjectHelper.cs
@@ -5,14 +5,21 @@ using UnityEditor;
 
 public static class GameObjectHelper {
 
-    /// <returns>return File reference</returns>
+    /// <summary>
+    /// Create a GameObject with the specified name and parent. eg:
+    /// <para>dir: "Assets/Res_Runtime"</para>
+    /// <para>fileName: "MyFileName"</para>
+    /// <para>ext: ".asset"</para>
+    /// </summary>
+    /// <returns>return the File reference</returns>
     public static T CreateFile_ScriptableObject<T>(string dir, string fileNameWithoutExt, string ext = ".asset") where T : ScriptableObject {
         var so = ScriptableObject.CreateInstance<T>();
         so.name = fileNameWithoutExt;
-        AssetDatabase.CreateAsset(so, dir + so.name + ext);
+        string file = Path.Combine(dir, so.name + ext);
+        AssetDatabase.CreateAsset(so, file);
         AssetDatabase.SaveAssets();
 
-        so = AssetDatabase.LoadAssetAtPath<T>(dir + so.name + ext);
+        so = AssetDatabase.LoadAssetAtPath<T>(file);
         return so;
     }
 
